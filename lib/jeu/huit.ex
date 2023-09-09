84966e3b72c98b737b7d2241995635164534fd6f
timtonix <timtonic@outlook.fr>
2023-09-09 18:26:17 +0200

    règles de base

--- lib/jeu/huit.ex
+++ lib/jeu/huit.ex
@@ -3,22 +3,17 @@ defmodule Jeu.Huit do
   Implémentation du jeu de 8 américain.
   """
   @behaviour Regles
-defstruct actuel: 0,
-défausse: Pile.new(),
-                   mains: %{},
-noms: %{},
-pioche: Pile.new(),
-            sens: :ordinaire,
-                   visible: nil
-                   @typedoc "Un jeu de 8 américain"
-                             @type t() :: %{
-                                          actuel: integer(),
-défausse: Pile.t(),
-mains: %{integer() => Pile.t()},
-noms: %{integer() => String.t()},
-                           pioche: Pile.t(),
-                                   sens: :ordinaire | :inverse,
-                                         visible: Carte.t()
-}
-# …
+defstruct actuel: 0, defausse: Pile.new(), mains: %{}, noms: %{},
+    pioche: Pile.new(), sens: ordinaire, visible: nil
+
+@typedoc "Un jeu de 8 américain"
+@type t() :: %{
+  actuel: integer(),
+defausse: Pile.t(),
+  mains: %{integer() => Pile.t()},
+  noms: %{integer(), String.t()}
+  pioche: String.t()
+sens: :ordinaire | :inverse,
+visible: Carte.t()
+             }
 end
