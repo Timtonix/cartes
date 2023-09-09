321c378ff65e50c76c581cd20b598eb9564d166c
timtonix <timtonic@outlook.fr>
2023-09-09 18:50:28 +0200

    le 8 américain

--- lib/jeu/huit.ex
+++ lib/jeu/huit.ex
@@ -3,15 +3,30 @@ defmodule Jeu.Huit do
   Implémentation du jeu de 8 américain.
   """
   @behaviour Regles
-defstruct actuel: 0, defausse: Pile.new(), mains: %{}, noms: %{},
-    pioche: Pile.new(), sens: ordinaire, visible: nil
+  defstruct actuel: 0, defausse: Pile.new(), mains: %{}, noms: %{},
+      pioche: Pile.new(), sens: ordinaire, visible: nil
 
-@typedoc "Un jeu de 8 américain"
-@type t() :: %{
-  actuel: integer(),
-  defausse: Pile.t(),
-  mains: %{integer() => Pile.t()},noms: %{integer(), String.t()},
-  pioche: String.t(),
-  sens: :ordinaire | :inverse,
-  visible: Carte.t()}
+  @typedoc "Un jeu de 8 américain"
+  @type t() :: %{
+    actuel: integer(),
+    defausse: Pile.t(),
+    mains: %{integer() => Pile.t()},noms: %{integer(), String.t()},
+    pioche: String.t(),
+    sens: :ordinaire | :inverse,
+    visible: Carte.t()
+               }
+  @impl true
+  @doc """
+     Retourne le titre du jeu sous la forme d’une chaîne de caractères.
+     """
+   @spec titre() :: String.t()
+   def titre(), do: "Le 8 américain"
+
+ @impl true
+  @doc """
+  Crée une nouvelle structure.
+  Cette fonction retourne la structure propre aux règles de jeu
+  (probablement définie dans le module spécifique).
+  """
+  @spec new() :: t()
 end
